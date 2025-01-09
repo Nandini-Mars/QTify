7dd0f96da63a4ea34c54f7b585ba683a494277ba
Nandini-Mars <nandini.mailbox@gmail.com>
2025-01-09 16:33:21 +0530

    Navbar

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,5 +3,5 @@ import LogoImage from "../../assets/logo.png";
 import "./LogoStyles.css"
 
 export default function Logo() {
-  return <img src={LogoImage} alt="logoImage" width={67}/>;
+  return <img className="logo" src={LogoImage} alt="logoImage" width={67}/>;
 }
