7c635dcb8d7935650d4cb9d01865a4d7a14dbb44
Nandini-Mars <nandini.mailbox@gmail.com>
2025-01-11 08:30:39 +0530

    HeroSection

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import "./HeroStyles.css";
+import HeroImage from "../../assets/vibrating-headphone.png"
 
 function Hero() {
   return (
     <div className="container">
+            <div>
+        <h1>100 Thousand Songs, ad-free</h1>
+        <h1>Over thousands podcast episodes</h1>
+      </div>
+      <div className="hero-image">
+        <img
+          src={HeroImage}
+          alt="headphones"
+        />
+      </div>
 
     </div>
   );
