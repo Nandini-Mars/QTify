3e66ca25bdbdca15b0448ad282ff30c6ffc3ef4b
Nandini-Mars <nandini.mailbox@gmail.com>
2025-01-16 15:21:12 +0530

    Navbar

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import { Card, CardContent, Typography, Chip } from "@mui/material";
 import "./CardStyle.css";
+import { useEffect, useState } from "react";
 
-const AlbumCard = () => {
-  // Dummy data
-  const album = {
-    name: "Dummy Album",
-    follows: 3500,
-    imageUrl: "https://qtify-backend-labs.crio.do/albums/top", // Dummy image URL
-  };
-
+const AlbumCard = ({
+    image,
+    follows,
+    title
+  }) => {
+  
   return (
     <div className="container">
       <div className="album-card">
-        <img className="album-image" src={album.imageUrl} alt={album.name} />
+        <img className="album-image" src={image} alt={title} />
         <CardContent className="album-content">
           <Typography variant="h6" component="div">
-            {album.name}
+            {title}
           </Typography>
           <Chip
-            label={`${album.follows} follows`}
+            label={`${follows} follows`}
             size="small"
             color="primary"
             className="follow-chip"
@@ -28,6 +27,37 @@ const AlbumCard = () => {
       </div>
     </div>
   );
-};
+}
+
+  const URL = "https://qtify-backend-labs.crio.do/albums/top";
+
+  function TopAlbum() {
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        const fetchAlbums = async () => {
+            try{
+                const response = await fetch(URL);
+                const apiData = await response.json();
+                console.log(apiData);
+                setData(apiData);
+            }catch(error){
+                console.error("Error ",error)
+            }
+        }
+        fetchAlbums();
+    }, [])
+
+    return (
+        <div className="container">
+          <div className="album-card">
+            {data.map((album) => (
+                <AlbumCard key={album.id } image={album.image} title={album.title} follows={album.follows}/>
+            ))}
+          </div>
+        </div>
+      );
+  }
+
 
 export default AlbumCard;
