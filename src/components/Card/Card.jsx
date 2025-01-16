43965a872e4a1997cf3811328b06b397225ce620
Nandini-Mars <nandini.mailbox@gmail.com>
2025-01-16 15:34:38 +0530

    Album

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Card, CardContent, Typography, Chip } from "@mui/material";
-import "./Card.css";
+import "./CardStyle.css";
 
 // AlbumCard component
 const AlbumCard = ({ image, follows, title }) => {
