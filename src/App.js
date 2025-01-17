b83af93f2c3efdd66fe2d837df298d972403463f
Nandini-Mars <nandini.mailbox@gmail.com>
2025-01-17 12:48:31 +0530

    Album

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Hero from './components/Hero/Hero.jsx';
-import TopAlbum from './components/Card/Card.jsx';
-import Section from '../Section/Section.jsx;'
+import Section from '.components/Section/Section.jsx';
 import "./Styles.css";
 
 function App() {
