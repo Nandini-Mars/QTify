0975e66ab88a19d3d9c475b320e24434cc50bdd7
Nandini-Mars <nandini.mailbox@gmail.com>
2025-01-22 21:57:44 +0530

    Section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,108 @@
-import React from "react";
-//import { Link } from "react-router-dom";
-//import Button from "../Button/Button";
-import Logo from "../Logo/Logo";
-import Search from "../Search/Search";
-import "./NavbarStyles.css";
-import Button from "../Button/Button";
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import Badge from '@mui/material/Badge';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MailIcon from '@mui/icons-material/Mail';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreIcon from '@mui/icons-material/MoreVert';
 
+import Logo from '../Logo/Logo';
 
-/*function Navbar({ searchData }) {
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+export default function PrimarySearchAppBar() {
+
   return (
-    <nav className={styles.navbar}>
-      <Link to="/">
-        <Logo />
-      </Link>
-      <Search
-        placeholder="Search a song of your choice"
-        searchData={searchData}
-      />
-      <Button>Give Feedback</Button>
-    </nav>
-  );
-}*/
-function Navbar() {
-  return (
-    <div className="container">
-    <nav className="navbar">
-      <Logo/>
-      <Search/>
-      <Button/>
-    </nav>
+    <div className='navbar'>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar sx={{ bgcolor:" #34c94b" }} position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <Logo/>
+          </IconButton>
+          
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+          <Box sx={{ flexGrow: 1 }} />
+          <button 
+          type="submit"
+          color="inherit"
+          style={{width:"150px",
+                  height:"36px",
+                  color:"#34c94b",
+                  backgroundColor: "#121212",
+                  borderRadius: "12px",
+                  cursor: "pointer",
+                  fontFamily: "'Poppins', sans-serif",
+                  fontWeight: "500"
+
+          }}>
+            Give Feedback
+            </button>
+        </Toolbar>
+      </AppBar>
+    </Box>
     </div>
   );
 }
-
-
-
-export default Navbar;
